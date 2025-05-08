0949a5b72597a0479a657192e5343ed35c636889
harirmahmoud <harir.walkz04@gmail.com>
2025-05-08 14:50:33 +0100

    first commentp

--- models/index.js
+++ models/index.js
@@ -11,7 +11,16 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'mysql',
+    dialectOptions: {
+      // Needed for some providers like PlanetScale
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }}
+  );
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
